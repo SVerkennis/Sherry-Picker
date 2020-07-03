cfd9e0305f77f041285178101517afeaf2280b52
Sabine Verkennis <s.verkennis@yahoo.de>
2020-07-03 11:08:45 +0200

    Build project structure

---
+++ backend/src/main/java/de/neuefische/cherrypicker/CherrypickerApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.cherrypicker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CherrypickerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CherrypickerApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/de/neuefische/cherrypicker/CherrypickerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.cherrypicker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CherrypickerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
