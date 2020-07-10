c5cd80ccffbcc3a6394962f2d2f5c860f41898a7
Sabine Verkennis <s.verkennis@yahoo.de>
2020-07-10 10:59:00 +0200

    Create frontend structure maincontent/header

--- frontend-cherrypicker/src/App.js
+++ frontend-cherrypicker/src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router} from 'react-router-dom';
+import Header from "./components/Header/Header";
+import MainContent from "./components/MainContent/MainContent";
 
 function App() {
   return (
     <div className="App">
+
+      <Router>
+        <Header/>
+        <MainContent/>
+      </Router>
+
+
 
     </div>
   );
---
+++ frontend-cherrypicker/src/components/MainContent/MainContent.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function MainContent() {
+    return (
+
+        <h1>Whaaaat is going on test?"</h1>
+
+    )
+}
