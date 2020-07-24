c50e169a57ad1bcbc8ac70ace9e5576744bdbe21
Sabine Verkennis <s.verkennis@yahoo.de>
2020-07-24 16:52:16 +0200

    Wischel den Code sauber

---
+++ frontend-cherrypicker/src/components/MainContent/BackgroundForm.js
@@ -0,0 +1,68 @@
+import React from "react";
+import {makeStyles} from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import {Route, Switch} from "react-router-dom";
+import FruitPage from "../FruitPage/FruitPage";
+import Apfel from "../FruitPage/Apfel";
+import Moehre from "../VegetablePage/Moehre";
+import VegetablePage from "../VegetablePage/VegetablePage";
+import RegisterPage from "../RegisterPage/RegisterPage";
+import LandingPage from "../LandingPage/LandingPage";
+import ShapeContent from "./ShapeContent";
+
+const useStyles = makeStyles((theme) => ({
+
+    shapeThing: {
+        backgroundRepeat: 'no-repeat',
+        backgroundColor: '#5CDB95',
+        marginLeft: '1.5em',
+        marginRight: '1.5em',
+        marginTop: '-1rem',
+        borderRadius: '22px',
+    }
+}));
+
+function BackgroundForm() {
+    const classes = useStyles();
+
+    return(
+<div className={classes.shapeThing}>
+    <Grid container>
+
+        <Switch>
+            <Route path="/fruits" exact>
+                <FruitPage/>
+            </Route>
+
+            <Route path="/apfel" exact>
+                <Apfel/>
+            </Route>
+
+            <Route path="/moehre" exact>
+                <Moehre/>
+            </Route>
+
+            <Route path="/vegetables" exact>
+                <VegetablePage/>
+            </Route>
+
+            <Route path="/register" exact>
+                <RegisterPage/>
+            </Route>
+
+            <Route path="/landing" exact>
+                <LandingPage/>
+            </Route>
+
+            <Route path="/">
+                <ShapeContent/>
+            </Route>
+        </Switch>
+
+    </Grid>
+</div>
+    )
+
+}
+
+export default BackgroundForm;
--- frontend-cherrypicker/src/components/MainContent/MainContent.js
+++ frontend-cherrypicker/src/components/MainContent/MainContent.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ShapeContent from "./ShapeContent";
 import Grid from "@material-ui/core/Grid";
-import { makeStyles } from '@material-ui/core/styles';
 import {Route, Switch} from "react-router-dom";
 import FruitPage from "../FruitPage/FruitPage";
 import Apfel from "../FruitPage/Apfel";
@@ -9,37 +8,15 @@ import VegetablePage from "../VegetablePage/VegetablePage";
 import RegisterPage from "../RegisterPage/RegisterPage";
 import LandingPage from "../LandingPage/LandingPage";
 import Moehre from "../VegetablePage/Moehre";
-
-const useStyles = makeStyles((theme) => ({
-
-    shapeThing: {
-        backgroundRepeat: 'no-repeat',
-      //  backgroundImage: 'url("images/backgroundShape.png")',
-      //  backgroundPosition: 'center',
-     //   backgroundSize: 'cover',
-        backgroundColor: '#5CDB95',
-        marginLeft: '1.5em',
-        marginRight: '1.5em',
-        marginTop: '-1rem',
-        borderRadius: '22px',
-      //  paddingBottom: '7.5rem',
-     //   clipPath: 'polygon(49%)',
-    },
-
-
-}));
+import BackgroundForm from "./BackgroundForm";
 
 export default function MainContent() {
-    const classes = useStyles();
-
     return (
 
 <>
-        <div className={classes.shapeThing}>
-        <Grid
-             container
-             className={classes.shapeContainer}
-        >
+        <BackgroundForm>
+
+        <Grid container>
 
             <Switch>
                 <Route path="/fruits" exact>
@@ -73,7 +50,9 @@ export default function MainContent() {
 
         </Grid>
 
-</div>
-    </>
+        </BackgroundForm>
+
+</>
+
     );
 }
